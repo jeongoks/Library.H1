24b22a6c312620941d87a9b1d51bdbe06c8cf175
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2020-12-10 20:01:25 +0100

    Added Console.Clear(); to CreateLoanee()

--- Library.H1/Library.cs
+++ Library.H1/Library.cs
@@ -51,6 +51,7 @@ namespace Library.H1
         /// </summary>
         public void CreateLoanee()
         {
+            Console.Clear();
             int result = 0;
             do
             {
